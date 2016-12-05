3773040ec7ac85647a941b6f94ca9f94cccbb891
Wurstnase <wurstnase.reprap@gmail.com>
2016-12-05 20:24:23 +0100

    Zweite LED blinkt

--- Arduino.ino
+++ Arduino.ino
@@ -1,12 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
   delay(100);
   digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
   delay(100);
 }
