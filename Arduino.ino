3aa038d9ad505f1e0de2c290978ed99a0ce92ba6
Wurstnase <wurstnase.reprap@gmail.com>
2016-12-05 20:07:04 +0100

    Pin 1 eingefügt Pin 1 auf HIGH gesetzt

--- Arduino.ino
+++ Arduino.ino
@@ -1,9 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(1, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(1, HIGH);
 }
